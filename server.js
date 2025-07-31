423da1c9265c846148a3fc951354f21fd53ea436
Muhammadelnaggar <nagarvet@hotmail.com>
2025-07-31 14:23:22 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,8 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'www')));
-const fs = require('fs');
-const path = require('path');
+
 
 // هذا الجزء يجيب كل الحيوانات المسجلة
 app.get('/api/animals', (req, res) => {
