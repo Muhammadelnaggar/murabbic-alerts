78cbf93e8d3924a97c8b4430204c2cac74d8ac2f
Muhammadelnaggar <nagarvet@hotmail.com>
2025-08-07 08:59:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const dataDir = path.join(__dirname, "data");
 
 app.use(cors());
 app.use(express.json());
