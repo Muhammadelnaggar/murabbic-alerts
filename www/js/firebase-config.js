a3a0e8b1214f794c1255f638b8d6b1aa562e246b
Muhammadelnaggar <nagarvet@hotmail.com>
2025-08-08 18:36:38 +0300

    Update firebase-config.js

--- www/js/firebase-config.js
+++ www/js/firebase-config.js
@@ -1,7 +1,16 @@
-// firebase-config.js (نسخة وهمية للنشر على GitHub)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-console.warn("⚠️ firebase-config.js هو ملف وهمي ولا يحتوي على إعدادات Firebase حقيقية.");
+const firebaseConfig = {
+  apiKey: "AIzaSyB0dtFS3R-MQ-LJfd_dB1YOTxiwDVshIYc",
+  authDomain: "murabbik.firebaseapp.com",
+  projectId: "murabbik",
+  storageBucket: "murabbik.firebasestorage.app",
+  messagingSenderId: "402719243568",
+  appId: "1:402719243568:web:631114a260d23202dd5cf5"
+};
 
-// تصدير قيم فارغة لتجنب كسر الكود
-export const db = null;
-export const auth = null;
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
