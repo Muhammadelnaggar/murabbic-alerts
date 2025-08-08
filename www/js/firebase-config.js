6c212ec1f28d5846e69acbd4787c6a62fd3c768c
Muhammadelnaggar <nagarvet@hotmail.com>
2025-08-08 18:27:14 +0300

    Create firebase-config.js

---
+++ www/js/firebase-config.js
@@ -0,0 +1,7 @@
+// firebase-config.js (نسخة وهمية للنشر على GitHub)
+
+console.warn("⚠️ firebase-config.js هو ملف وهمي ولا يحتوي على إعدادات Firebase حقيقية.");
+
+// تصدير قيم فارغة لتجنب كسر الكود
+export const db = null;
+export const auth = null;
