2c90b9deb4efd0f39af1fa8bb51cbacea9c2b5e1
Muhammad elnaggar <nagarvet78@gmail.com>
2025-08-08 12:36:45 +0300

    Removed public folder

---
+++ www/js/firebase-config.js
@@ -0,0 +1,12 @@
+// js/firebase-config.js
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCE5dErkgqVly7wOdmr7MDuDsU10WXXFc0",
+  authDomain: "your-project-id.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project-id.appspot.com",
+  messagingSenderId: "your-messaging-sender-id",
+  appId: "your-app-id"
+};
+
+firebase.initializeApp(firebaseConfig);
