b54f3a504748c6879a0c23d855beb9e4f0197e78
Muhammadelnaggar <nagarvet@hotmail.com>
2025-08-08 10:17:13 +0300

    Create config.js

---
+++ www/config.js
@@ -0,0 +1,9 @@
+// www/config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyC4tZ_gX9uhyD-dFcS_x81ArfjcL9-8olY",
+  authDomain: "murabbik.firebaseapp.com",
+  projectId: "murabbik",
+  storageBucket: "murabbik.appspot.com",
+  messagingSenderId: "402719243568",
+  appId: "1:402719243568:web:631114a260d23202dd5cf5"
+};
