91d899df80e9a30e37707e7ad33af986ba1caff5
Muhammadelnaggar <nagarvet@hotmail.com>
2025-08-11 10:52:13 +0300

    Update firebase-config.js

--- www/js/firebase-config.js
+++ www/js/firebase-config.js
@@ -1,8 +1,19 @@
-const firebaseConfig = {
+// www/js/firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore }   from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getAuth }        from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+// نحتاج export عشان الاستيراد الديناميكي يلاقيه
+export const firebaseConfig = {
   apiKey: "AIzaSyB0dtFS3R-MQ-LJfd_dB1YOTxiwDVshIYc",
   authDomain: "murabbik.firebaseapp.com",
   projectId: "murabbik",
-  storageBucket: "murabbik.firebasestorage.app",
+  storageBucket: "murabbik.appspot.com", // ← دي الصح لاسم البكت
   messagingSenderId: "402719243568",
   appId: "1:402719243568:web:631114a260d23202dd5cf5"
 };
+
+const app  = initializeApp(firebaseConfig);
+export const db   = getFirestore(app);
+export const auth = getAuth(app);
+export { app };
