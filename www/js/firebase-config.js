30f30e60ee2212b2550e9a423bc2290815b40120
Muhammadelnaggar <nagarvet@hotmail.com>
2025-08-26 18:42:01 +0300

    Update firebase-config.js

--- www/js/firebase-config.js
+++ www/js/firebase-config.js
@@ -1,19 +1,15 @@
-// www/js/firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore }   from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import { getAuth }        from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+// /js/firebase-config.js
+import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth, setPersistence, browserLocalPersistence } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// نحتاج export عشان الاستيراد الديناميكي يلاقيه
-export const firebaseConfig = {
-  apiKey: "AIzaSyB0dtFS3R-MQ-LJfd_dB1YOTxiwDVshIYc",
-  authDomain: "murabbik.firebaseapp.com",
-  projectId: "murabbik",
-  storageBucket: "murabbik.appspot.com", // ← دي الصح لاسم البكت
-  messagingSenderId: "402719243568",
-  appId: "1:402719243568:web:631114a260d23202dd5cf5"
-};
+// ← حط إعدادات مشروعك هنا
+const firebaseConfig = { /* ... */ };
 
-const app  = initializeApp(firebaseConfig);
-export const db   = getFirestore(app);
+const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
-export { app };
+// 👈 أهم سطر: خزّن الجلسة في localStorage (أكتر ثباتًا على الموبايل من IndexedDB)
+await setPersistence(auth, browserLocalPersistence).catch(console.warn);
+
+export const db = getFirestore(app);
